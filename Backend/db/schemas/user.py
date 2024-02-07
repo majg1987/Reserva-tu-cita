078786328702c8cf34bec2717d96d796108a54d8
Miguel Angel Jurado <majg1987@hotmail.com>
2024-02-07 21:57:02 +0100

    user schema created

---
+++ Backend/db/schemas/user.py
@@ -0,0 +1,7 @@
+def user_schema(user) -> dict:
+    return {"id": str(user["_id"]),
+            "email": str(user["email"]),
+            "firstname": str(user["firstname"]),
+            "lastname": str(user["lasname"]),
+            "phone": int(user["phone"])
+            }
