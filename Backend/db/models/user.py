8a21fc24c37b4b2897eda1303873f3a3693fe162
Miguel Angel Jurado <majg1987@hotmail.com>
2024-04-17 20:10:30 +0200

    add user model

--- Backend/db/models/user.py
+++ Backend/db/models/user.py
@@ -2,7 +2,12 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     id: str | None = None
+    username: str
     email: str
     firstname: str | None = None
     lastname: str | None = None
-    phone: int | None = None
+    phone: str | None = None
+    disabled: bool
+
+class UserInDB(User):
+    password: str
