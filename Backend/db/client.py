706c692cbd83e9f62191dfe3785a76b850d85112
Miguel Angel Jurado <majg1987@hotmail.com>
2024-04-17 20:04:36 +0200

    modify database connection

--- Backend/db/client.py
+++ Backend/db/client.py
@@ -4,6 +4,6 @@ from pymongo import MongoClient
 
 load_dotenv()
 
-user_db = os.getenv("USER_DB")
+USER_DB = os.getenv("USER_DB")
 
-db_client = MongoClient(os.getenv("DATABASE_URL")).user_db
+db_client = MongoClient(os.getenv("DATABASE_URL")).USER_DB
